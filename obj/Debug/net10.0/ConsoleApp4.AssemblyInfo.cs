df882ef97037af2f4d98d1584527a4cbb9f3e0f1
concutocuc123-max <concutocuc123@gmail.com>
2026-03-28 10:41:22 +0700

    Clean up: ignore build artifacts and temporary files

--- obj/Debug/net10.0/ConsoleApp4.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+531fca465856456dd7abb4f74a3e86b15804d89f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2786355b922b658db3bb7d50c95a1eb8f757972")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
